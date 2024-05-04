2c15291bab6c64fd3be30ee31c134e9478a94001
Ollldman <dodrolla@gmail.com>
2024-05-04 20:52:44 +0300

    from windows

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main(){
     string b {"Noe noe !! "};
     string a {"Hello world!"};
     string c {"Youp!"};
+    string G {"from Windows"};
     cout<<a+b+c<<endl;
     cout<<c+a<<endl;
+    cout<<G<<endl;
     return 0;
 }
