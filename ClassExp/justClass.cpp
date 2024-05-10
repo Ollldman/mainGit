223bf2477313e25b995f16cab8b78ff03f5ac5bd
Ollldman <dodrolla@gmail.com>
2024-05-10 18:49:19 +0300

    new

---
+++ ClassExp/justClass.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Calculator
+{
+    private:
+        int day,
+            month,
+            year;
+    public:
+        void message(){
+            cout<<"Messege of calculator\n";
+      }
+         double stepen(double a, double b){
+            return pow(a,b);
+      }
+        void setDate(int date_day, int date_month, int date_year){
+            day = date_day;
+            month = date_month;
+            year = date_year;
+        }
+        void getDate(){
+            cout<<"Date: "<<day<<"."<<month<<"."<<year<<"\n";
+        }
+};
+
+int main(){
+    double a = 3.13;
+    double b = 4.12;
+    int day,month,year;
+    cout<<"Enter date: ";
+    cin>>day>>month>>year;
+    Calculator objCalc;
+    objCalc.message();
+    cout<<"pow: "<<objCalc.stepen(a,b)<<endl;
+    objCalc.setDate(day, month, year);
+    objCalc.getDate();
+    return 0;
+}
